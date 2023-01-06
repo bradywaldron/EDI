081cfd1de1b4109e9f8ed1d8c208fac04d260c4a
bradywaldron <bradywaldron17@gmail.com>
2023-01-06 09:55:54 -0500

    CrativeCoding5

---
+++ CreativeCoding/EmptyExample2/sketch.js
@@ -0,0 +1,10 @@
+function setup() {//runs once
+createCanvas(720,720);//creates a Canvas 720px by 720px
+}
+
+function draw() {//runs in a loop
+quad(0,0,
+     300,0,
+     300,300,
+     0,300);
+}
