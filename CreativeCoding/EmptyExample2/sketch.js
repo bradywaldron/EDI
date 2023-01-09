fc4a2cc305d7afcebed8b14e36517f606d6b6b68
bradywaldron <bradywaldron17@gmail.com>
2023-01-09 12:53:20 -0500

    CreativeCode 6

--- CreativeCoding/EmptyExample2/sketch.js
+++ CreativeCoding/EmptyExample2/sketch.js
@@ -1,10 +1,5 @@
 function setup() {//runs once
-createCanvas(720,720);//creates a Canvas 720px by 720px
 }
 
 function draw() {//runs in a loop
-quad(0,0,
-     300,0,
-     300,300,
-     0,300);
 }
